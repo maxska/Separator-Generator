283aa780f5949dd9ff887b5a3b971d233fa8a6b2
Max <maxskan98@gmail.com>
2021-02-07 15:37:11 +0100

    improved code structure

--- gui.py
+++ gui.py
@@ -1,63 +1,69 @@
 from tkinter import *
 
 
+###############################################################################
+#								Functions
+###############################################################################
+
 def generate():
-	pass
+    pass
 
 
 def comment_input_placeholder(event):
-	""" Adds or removes placeholder to the comment_input Entry. """
-	text = comment_input_text.get()
+    """ Adds or removes placeholder to the comment_input Entry. """
+    text = comment_text.get()
 
-	
-	if text == placeholder:
-		print("B")
-		comment_input_text.set("")
-	if len(text) == 0:
-		comment_input_text.set(placeholder)
+    if text == placeholder:
+        comment_text.set("")
+    elif len(text) == 0:
+        comment_text.set(placeholder)
 
-	return
+    return
 
+
+###############################################################################
+#							Creating GUI elements
+###############################################################################
 
 root = Tk()
 root.title("Separator Generator")
 
-language_label = Label(root, text="Line comment syntax: ")
-language_label.pack()
+language_label = Label(root, text="Line comment syntax:")
 
 
-comment_input_text = StringVar()
+comment_text = StringVar()
 comment_input = Entry(
-	root, 
-	width=50,
-	fg="grey",
-	textvariable=comment_input_text
+    root,
+    width=30,
+    fg="grey",
+	# the text in comment_input can now be accessed through comment_text:
+    textvariable=comment_text  
 )
-comment_input.pack()
-
 placeholder = "e.g. '//' in C++ or '#' in Python"
-comment_input_text.set(placeholder)
+comment_text.set(placeholder)
+# Binds comment_input_placeholder function to when the user clicks on or 
+# outside of comment_input:
 comment_input.bind('<FocusIn>', comment_input_placeholder)
 comment_input.bind('<FocusOut>', comment_input_placeholder)
 
-
-
-
-temp_entry = Entry(root)
-temp_entry.pack()
-
-
-
-
+temp_input = Entry(root)
 
 generate_button = Button(
-	root, 
-	text="Generate separator",
-	pady=50, 
-	bg="#002060",
-	command=generate
+    root,
+    text="Generate separator",
+    pady=50,
+    bg="#002060",
+    command=generate
 )
-						 
+
+
+###############################################################################
+#						Placing the elements in the window
+###############################################################################
+
+language_label.pack()
+comment_input.pack()
+temp_input.pack()
 generate_button.pack()
 
 
