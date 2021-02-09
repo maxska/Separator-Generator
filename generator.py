c3e940fc1a6ab75920e0f7ab1e298cbb4e717196
Max <maxskan98@gmail.com>
2021-02-09 18:13:26 +0100

    fixed compensation of uneven len(title) and max_length

--- generator.py
+++ generator.py
@@ -14,6 +14,10 @@ def get_side_separator_length(maximum_spacing, preferred_spacing):
 	return maximum_spacing - preferred_spacing
 
 
+def is_uneven(number):
+	return number/2 != number//2
+
+
 def generate(line_comment, separator, total_width, rows, title, 
 			 preferred_spacing):
 	""" Generates and returns a separator. """
@@ -35,25 +39,32 @@ def generate(line_comment, separator, total_width, rows, title,
 
 	side_separator = separator * side_separator_length
 	side_spacing = " " * preferred_spacing
-	compensate_uneven_side = separator if len(title)/2 != len(title)//2 else ""
+
+	# compensate with extra separator symbol if one of len(title) and
+	# total_width is uneven and not the other:
+	compensate_uneven_title \
+		= separator if is_uneven(len(title)) and not is_uneven(total_width) else ""
+
+	compensate_uneven_length \
+		= separator if is_uneven(total_width) and not is_uneven(len(title)) else ""
 
 	result = ""
 	result += above_and_below_separators
 	result += f"{line_comment}{side_separator}{side_spacing}"
 	result += f"{title}{side_spacing}{side_separator}"
-	result += f"{compensate_uneven_side}\n"
+	result += f"{compensate_uneven_title}{compensate_uneven_length}\n"
 	result += above_and_below_separators	
 	return result
 
-
 print(
-	generate("//", "#", 80, 3, "The title.", 5)
+	generate("//", "#", 80 , 3, "abc", 5)
 )
-
 print(
-	generate("//", "#", 80, 3, "The title..", 5)
+	generate("//", "#", 80 , 3, "abcd", 5)
 )
-
 print(
-	generate("//", "#", 80, 3, "The title...", 5)
+	generate("//", "#", 81 , 3, "abc", 5)
+)
+print(
+	generate("//", "#", 81 , 3, "abcd", 5)
 )
